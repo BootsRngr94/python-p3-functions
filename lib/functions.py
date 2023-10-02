df6d29b222dade9773c0752ed3cede0cdfd58121
Luis Westberry <takuame7@gmail.com>
2023-10-02 14:28:47 -0500

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(Guido):
+    print(f"Hello, {Guido}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+    #greet_with_default()
+    #greet_with_default("Sunny")
     pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    
 
 def halve(number):
-    pass
+    if not isinstance(number, (int,float)):
+        return None
+
+    return number / 2
